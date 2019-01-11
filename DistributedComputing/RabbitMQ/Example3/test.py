e49f90c4fdd70b478c26a1b323aff1e1975e6307
Ubuntu <ubuntu@ip-172-31-5-168.us-east-2.compute.internal>
2019-01-11 04:55:49 +0000

    remote job

--- DistributedComputing/RabbitMQ/Example3/test.py
+++ DistributedComputing/RabbitMQ/Example3/test.py
@@ -1,5 +1,7 @@
 import celery
 
+# First we setup a connection to the message broker:
+
 app = celery.Celery('test',
                         broker='amqp://myguest:myguestpwd@PROD-JOB-844fd7d2202ac4da.elb.us-east-2.amazonaws.com',
                         backend='amqp://myguest:myguestpwd@PROD-JOB-844fd7d2202ac4da.elb.us-east-2.amazonaws.com')
@@ -7,3 +9,12 @@ app = celery.Celery('test',
 @app.task
 def echo(message):
     return message
+
+#run this code with "celery -A test worker --loglevel=info". 
+#Then this machine will become a worker, and will be able to run the app task, i.e. the echo function, whenever the broker requests it.
+
+# The echo function will be run by these commands on a remote machine:
+
+# from test import echo
+# res = echo.delay('Python rocks!'); print(type(res)); print(res)
+# res.result
