bfa17bc38ea6285168ac0fb6d06c41fcd8959790
Ubuntu <ubuntu@ip-172-31-5-168.us-east-2.compute.internal>
2019-01-11 06:26:11 +0000

    mergesort

---
+++ DistributedComputing/RabbitMQ/Example4/mergesort.py
@@ -0,0 +1,39 @@
+import celery
+
+
+app = celery.Celery('mergesort',
+                        broker='amqp://myguest:myguestpwd@PROD-JOB-844fd7d2202ac4da.elb.us-east-2.amazonaws.com',
+                        backend='amqp://myguest:myguestpwd@PROD-JOB-844fd7d2202ac4da.elb.us-east-2.amazonaws.com')
+
+
+#run this code with "celery -A mergesort worker --loglevel=info". 
+#Then this machine will become a worker, and will be able to run the app task, i.e. the sort function, whenever the broker requests it.
+
+
+@app.task
+def sort(xs):
+    lenxs = len(xs)
+    if(lenxs <= 1):
+        return(xs)
+
+    half_lenxs = lenxs // 2
+    left = xs[:half_lenxs]
+    right = xs[half_lenxs:]
+    return(merge(sort(left), sort(right)))
+
+
+def merge(left, right):
+    nleft = len(left)
+    nright = len(right)
+
+    merged = []
+    i = 0
+    j = 0
+    while i < nleft and j < nright:
+        if(left[i] < right[j]):
+            merged.append(left[i])
+            i += 1
+        else:
+            merged.append(right[j])
+            j += 1
+    return merged + left[i:] + right[j:]
