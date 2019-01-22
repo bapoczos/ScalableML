4b7436cf363b7d1fd2351f1853af744a45876525
Ubuntu <ubuntu@ip-172-31-12-193.us-east-2.compute.internal>
2019-01-22 17:14:10 +0000

    book codes removed

--- DistributedComputing/DistributedComputingWithPython_Code/Chapter 2/currency/generators.py
+++
@@ -1,8 +0,0 @@
-def mygenerator(n):
-    while n:
-        n -= 1
-        yield n
-
-if __name__ == '__main__':
-    for i in mygenerator(3):
-        print(i)
--- DistributedComputing/DistributedComputingWithPython_Code/Chapter 6/pbs/simple/simple.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/usr/local/bin/python3.5 -c "print('Hello, HTCondor!')"
