6ec720d5b5fa553704680b4b8fa1842537a17311
EC2 Default User <ec2-user@ip-172-31-2-52.us-east-2.compute.internal>
2019-02-12 06:48:34 +0000

    jupyter setup

---
+++ PySparkDemos/jupyter_setup.sh
@@ -0,0 +1,7 @@
+export spark_master_hostname=ec2-18-191-239-57.us-east-2.compute.amazonaws.com
+
+export memory=11000M 
+
+export PYTHONPATH=$SPARK_HOME/python:$SPARK_HOME/python/lib/py4j-0.10.7-src.zip:$PYTHONPATH
+
+PYSPARK_DRIVER_PYTHON=jupyter PYSPARK_DRIVER_PYTHON_OPTS="notebook --no-browser --port=7777" pyspark --master spark://$spark_master_hostname:7077 --executor-memory $memory --driver-memory $memory
