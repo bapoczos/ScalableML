8b74910d1bbc8709a3cac7a69a1d44867dbfce91
Ubuntu <ubuntu@ip-172-31-3-75.us-east-2.compute.internal>
2019-09-06 14:56:13 +0000

    some comments added

--- DistributedComputing/RabbitMQ/Example_Celery_1/test.py
+++ DistributedComputing/RabbitMQ/Example_Celery_1/test.py
@@ -2,13 +2,18 @@ import celery
 
 # First we setup a connection to the message broker:
 
+# Make sure that the 'myguest' user exists with 'myguestpwd' on the RabbitMQ server, and the 
+# 'PROD-JOB-844fd7d2202ac4da.elb.us-east-2.amazonaws.com' address from the load balancer has been set up correctly.
+
 app = celery.Celery('test',
                         broker='amqp://myguest:myguestpwd@PROD-JOB-844fd7d2202ac4da.elb.us-east-2.amazonaws.com',
                         backend='amqp://myguest:myguestpwd@PROD-JOB-844fd7d2202ac4da.elb.us-east-2.amazonaws.com')
 
+# Let us use the @app.task decorator on our echo function. It will allow us to call echo.delay() on the server remotely
+
 @app.task
 def echo(message):
-    return message
+    return 'Response from worker: '+message
 
 #run this code with "celery -A test worker --loglevel=info". 
 #Then this machine will become a worker, and will be able to run the app task, i.e. the echo function, whenever the broker requests it.
