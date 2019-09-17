d7f2830325c69120e7709e2266868cc56724c5a5
Ubuntu <ubuntu@ip-172-31-14-37.us-east-2.compute.internal>
2019-09-17 05:59:15 +0000

    files renmaed

---
+++ DistributedComputing/RabbitMQ/Example_private_message/utils.py
@@ -0,0 +1,10 @@
+import numpy as np
+import json
+
+class NumpyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        elif isinstance(obj, np.int64): 
+            return int(obj)
+        return json.JSONEncoder.default(self, obj)
