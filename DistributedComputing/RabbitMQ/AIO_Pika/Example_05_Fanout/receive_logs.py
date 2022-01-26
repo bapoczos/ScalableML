9a721163fca9f6a177c647e1d70ad6bfa8ad4c3e
Ubuntu <ubuntu@ip-172-31-44-231.us-east-2.compute.internal>
2022-01-26 08:08:11 +0000

    fanout

---
+++ DistributedComputing/RabbitMQ/AIO_Pika/Example_05_Fanout/receive_logs.py
@@ -0,0 +1,46 @@
+import asyncio
+from aio_pika import connect, IncomingMessage, ExchangeType
+
+loop = asyncio.get_event_loop()
+
+
+def on_message(message: IncomingMessage):
+    with message.process():
+        print("[x] %r" % message.body)
+
+
+async def main():
+    # Perform connection
+    connection = await connect(
+        #"amqp://guest:guest@localhost/", 
+        "amqp://myguest:myguestpwd@RabbitMQLB-8e09cd48a60c9a1e.elb.us-east-2.amazonaws.com/",
+        loop=loop
+    )
+
+    # Creating a channel
+    channel = await connection.channel()
+    await channel.set_qos(prefetch_count=1)
+
+    logs_exchange = await channel.declare_exchange(
+        'logs',
+        ExchangeType.FANOUT
+    )
+
+    # Declaring queue
+    queue = await channel.declare_queue(exclusive=True)
+
+    # Binding the queue to the exchange
+    await queue.bind(logs_exchange)
+
+    # Start listening the queue with name 'task_queue'
+    await queue.consume(on_message)
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.create_task(main())
+
+    # we enter a never-ending loop that waits for data
+    # and runs callbacks whenever necessary.
+    print(' [*] Waiting for logs. To exit press CTRL+C')
+    loop.run_forever()
